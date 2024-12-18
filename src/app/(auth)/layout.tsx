8facda1eb0947a39852ff79cf73189072975f471
pyfuse <hauwamuktar880@gmail.com>
2024-12-19 00:02:34 +0100

    fixed auth page layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,10 +7,8 @@ import { ReactNode } from "react";
 const layout = ({ children }: { children: ReactNode }) => {
   return (
     <ToastContextProvider>
-      <div className="container py-10  h-[calc(100dvh-56px)] mx-auto px-4 overflow-y-auto">
-        <ToastContainer />
-        {children}
-      </div>
+      <ToastContainer />
+      {children}
     </ToastContextProvider>
   );
 };
