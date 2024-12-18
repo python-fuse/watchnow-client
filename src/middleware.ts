eca705d776711b3009e96229d9f17f9b92734a2d
pyfuse <hauwamuktar880@gmail.com>
2024-12-18 17:11:48 +0100

    middleware strategy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
+import AuthService from "./lib/AuthService";
 
 export const middleware = async (req: NextRequest) => {
   const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
   try {
-    const res = await fetch(`${BACKEND_URL}/api/auth/check-auth`, {
-      method: "GET",
-      headers: {
-        Cookie: req.headers.get("cookie") || "", // Pass cookies from the request
-      },
-    });
+    const res = await AuthService.checkAuth();
 
     if (res.status !== 200) {
       throw new Error("Unauthorized");
